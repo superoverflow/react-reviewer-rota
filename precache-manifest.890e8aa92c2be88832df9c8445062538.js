6c97eb70828d80bb7a8d0a91b757b3764f3cca39
cyrus.tang <cyrus.tang@revolut.com>
2019-11-25 17:41:18 +0000

    Updates

---
+++ precache-manifest.890e8aa92c2be88832df9c8445062538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644022d60da5e30593d981d85837e240",
+    "url": "/react-reviewer-rota/index.html"
+  },
+  {
+    "revision": "d06c5bd55e8cfdb9faa9",
+    "url": "/react-reviewer-rota/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4fc57629e91292c78ed3",
+    "url": "/react-reviewer-rota/static/css/main.d9fb3742.chunk.css"
+  },
+  {
+    "revision": "d06c5bd55e8cfdb9faa9",
+    "url": "/react-reviewer-rota/static/js/2.128d6641.chunk.js"
+  },
+  {
+    "revision": "4fc57629e91292c78ed3",
+    "url": "/react-reviewer-rota/static/js/main.5ff41b5b.chunk.js"
+  },
+  {
+    "revision": "badd964b2efcbdf7a82b",
+    "url": "/react-reviewer-rota/static/js/runtime-main.73032b27.js"
+  }
+]);
